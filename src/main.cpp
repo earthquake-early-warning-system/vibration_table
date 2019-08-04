be5cbe20676bf59550ff23dcd9cd8b827c34a6f7
rahuldeo2047 <rahuldeo2047@gmail.com>
2019-08-05 00:58:31 +0530

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -21,56 +21,171 @@ void _loop()
   delay(1000);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
+  // wait for a second
   delay(1000);
 }
 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo myservo; // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-
-void setup() {
-  myservo.attach(D7);  // attaches the servo on GIO2 to the servo object
+void setup()
+{
+  myservo.attach(D7); // attaches the servo on GIO2 to the servo object
 }
+
+#define MAX_SIZE_SIN (11)
+float inc_sin[MAX_SIZE_SIN] =
+    {
+        1.5192246987792,
+        1.23116594048622,
+        0.973193125842964,
+        0.745384835867802,
+        0.547810463172671,
+        0.380530190825445,
+        0.24359497401758,
+        0.137046524542617,
+        0.060917298090424,
+        0.015230484360873,
+        0};
 
 void vibrate(float freq_hz, float amp)
 {
-  static elapsedMillis elapsedTime;
-  static bool alternate=false;
+  static elapsedMillis elapsedTime = 0;
+  static bool alternate = true;
   const float pos = 90.0f;
-  static float interval = 1000.0f/freq_hz;
-  
+  static float interval;
+  static float inc = 0.0;
+  const float res = 0.001;
+  const float inc_res = 0.01;
+  float angle = 0.0f;
+  static float index = 0;
+
+  interval = (2000.0f / freq_hz) * res;
+
   if (elapsedTime >= interval)
   {
     elapsedTime = 0;
-    if(alternate)
+
+    if (alternate == true)
     {
-      myservo.write(pos + amp);
+      inc += inc_res; //index MAX_SIZE_SIN'
+      angle = pos + sin(pos + inc) * 10.0f;
+      if (angle > pos + amp)
+      {
+        alternate = false;
+      }
     }
-    else
+
+    if (alternate == false)
     {
-      myservo.write(pos - amp);
+      inc -= inc_res; //index MAX_SIZE_SIN
+      angle = pos + sin(pos + inc) * 10.0f;
+      if (angle < pos - amp)
+      {
+        alternate = true;
+      }
     }
-    alternate = (alternate == true) ? false : true;
-    //delay();
-    
-    
+    myservo.write(angle);
   }
-   
+}
+void __vibrate(float freq_hz, float amp)
+{
+  static elapsedMillis elapsedTime = 0;
+  static bool alternate = true;
+  const float pos = 90.0f;
+  static float interval;
+  static float inc = 0.0;
+  const float res = 0.001;
+  const float inc_res = 0.01;
+  float angle = 0.0f;
+  static float index = 0;
+
+  interval = (2000.0f / freq_hz) * res;
+
+  if (elapsedTime >= interval)
+  {
+    elapsedTime = 0;
+
+    if (alternate == true)
+    {
+      inc += inc_res; //index MAX_SIZE_SIN'
+      angle = pos + sin(pos + inc) * 10.0f;
+      if (angle > pos + amp)
+      {
+        alternate = false;
+      }
+    }
+
+    if (alternate == false)
+    {
+      inc -= inc_res; //index MAX_SIZE_SIN
+      angle = pos + sin(pos + inc) * 10.0f;
+      if (angle < pos - amp)
+      {
+        alternate = true;
+      }
+    }
+    myservo.write(angle);
+  }
+}
+void _vibrate(float freq_hz, float amp)
+{
+  static elapsedMillis elapsedTime = 0;
+  static bool alternate = true;
+  const float pos = 90.0f;
+  static float interval;
+  static float inc = 0.0;
+  const float res = 1;
+  float angle = 0.0f;
+  static float index = 0;
+
+  interval = 1000.0f / freq_hz / MAX_SIZE_SIN;
+
+  if (elapsedTime >= interval * res)
+  {
+    elapsedTime = 0;
+
+    alternate = index > MAX_SIZE_SIN ? false : alternate;
+    alternate = index < 0 ? true : alternate;
+
+    if (alternate == true)
+    {
+      index += res; //index MAX_SIZE_SIN
+    }
+
+    if (alternate == false)
+    {
+      index -= res; //index MAX_SIZE_SIN
+    }
+
+    inc = inc_sin[(int)index] * 10.0f;
+
+    angle = pos + inc;
+
+    myservo.write(angle);
+  }
 }
 
-
 int const time_ms = 500;
-void loop() {
-   
+void loop()
+{
 
-  vibrate(1, 10);
-  // for (pos =0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  //vibrate(4, 50);
+
+  for (float inc = 0; inc < 360; inc += 1)
+  { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(90 + sin(radians(inc))*10); // tell servo to go to position in variable 'pos'
+    delay(1);               // waits 15ms for the servo to reach the position
+  }
+
+  // for (float inc = 360; inc >= 0; inc -= 1)
+  // { // goes from 0 degrees to 180 degrees
   //   // in steps of 1 degree
-  //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
-  //   delay(80);                       // waits 15ms for the servo to reach the position
+  //   myservo.write(90 + sin(radians(90 + inc))*10); // tell servo to go to position in variable 'pos'
+  //   delay(10);               // waits 15ms for the servo to reach the position
   // }
   //delay(1000);
   // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
