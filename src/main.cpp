685a8b7bd7e4f9e5f8d600c5ede9a311992be486
rahuldeo2047 <rahuldeo2047@gmail.com>
2019-08-04 22:16:06 +0530

    freq and amp crude vibration

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "Arduino.h"
+#include <elapsedMillis.h>
 
 void _setup()
 {
@@ -34,27 +35,57 @@ void setup() {
   myservo.attach(D7);  // attaches the servo on GIO2 to the servo object
 }
 
+void vibrate(float freq_hz, float amp)
+{
+  static elapsedMillis elapsedTime;
+  static bool alternate=false;
+  const float pos = 90.0f;
+  static float angle = pos;
+  static float interval = 1000.0f/freq_hz;
+  
+  //if(alternate 
+  // angle += alternate == true ? 0.1 : -0.1;
+
+  // if(angle >= pos+amp)
+  // {
+  //   alternate = false;
+  // }
+
+  // if (elapsedTime >= interval)
+  // {
+  //   elapsedTime = 0;
+    
+
+
+    if(alternate)
+    {
+      //alternate = false;
+      myservo.write(pos + amp);
+    }
+    else
+    {
+      //alternate = true;
+      myservo.write(pos - amp);
+    }
+    //delay();
+    
+    alternate = alternate == true ? false : true;
+  }
+   
+}
+
+
 int const time_ms = 500;
 void loop() {
-  int pos;
+   
 
-  myservo.write(0);
-  delay(time_ms);
-  myservo.write(90);
-  delay(time_ms);
-  myservo.write(180);
-  delay(time_ms);
-  myservo.write(0);
-  delay(time_ms);
-  myservo.write(180);
-  delay(time_ms);
-
+  vibrate(1, 10);
   // for (pos =0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
   //   // in steps of 1 degree
   //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
   //   delay(80);                       // waits 15ms for the servo to reach the position
   // }
-  delay(1000);
+  //delay(1000);
   // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
   //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
   //   delay(15);                       // waits 15ms for the servo to reach the position
